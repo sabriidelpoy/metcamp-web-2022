d267600256129ab5c71ed0b52dae677c05d6c917
Sabrina del Poy <sabrina.delpoy@auctane.com>
2022-07-19 18:43:41 +0200

    fix routes

--- src/opcion3/App.js
+++ src/opcion3/App.js
@@ -1,24 +1,21 @@
 import React from "react";
 import {
-  BrowserRouter as Router,
+  BrowserRouter,
   Routes,
   Route
 } from "react-router-dom";
-import {Home} from './Home';
-import {Game} from './Game'
+import { Home } from './Home';
+import { Game } from './Game'
 
 import 'bulma/css/bulma.min.css';
 
 export default function App() {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/game" element={<Game/>}/>
-          <Route path="/metcamp-web-2022" element={<Home />}/>
-        </Routes>
-      </Router>
-    </div>
+    <BrowserRouter basename={"metcamp-web-2022"}>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/game" element={<Game/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
