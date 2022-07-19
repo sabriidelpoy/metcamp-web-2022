022068b60d86a8d8ed7a5b5e844ed66a3471b662
Sabrina del Poy <sabrina.delpoy@auctane.com>
2022-07-19 19:08:46 +0200

    agrego componente boton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
 
 function App() {
+  const onClickHandler = () => {
+    alert('chau');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button text={"Hacé Click"} onClick={()=> alert('hola')} />
+      <Button text={"Otro botón para hacer Click"} onClick={onClickHandler} />
     </div>
   );
 }
