0fa9308598647211af79e502f97d269526f5b3da
Sabrina del Poy <sabrina.delpoy@auctane.com>
2022-07-19 18:56:47 +0200

    create app

--- src/opcion3/Button.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Button({ text, onClickHandler }) {
-    return (
-        <button onClick={onClickHandler} className="button is-primary">
-            { text }
-        </button>
-    )
-}
